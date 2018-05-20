490a566976d73b6e4ec2c36bf311ae7babbcfddf
mlazaric <marko.lazaric@gmail.com>
2018-05-20 13:16:17 +0200

    Preuredio Kata rješenje i zakomentirao stvari

--- Kata/kata.c
+++ Kata/kata.c
@@ -9,26 +9,36 @@ int main(void)
 	for (i = 0; i < n; ++i)
 	{
 		scanf("%d", br + i);
-		fakt = fakt * (i + 1);
 	}
 	
 	scanf("%d", &k);
-	fakt = fakt / n;
+	
+	/* Izračunaj (n - 1)! */
+	for (i = 1; i < n; ++i)
+	{
+		fakt = fakt * i;
+	}
+	
+	/* U zadatku nam je prva permutacija 1, ali nam je smislenije početi od 0 */
 	k--;
 	
 	for (i = n - 1; i > 0; --i)
 	{
+		/* Biramo znamenku jednu po jednu */
 		printf("%d ", br[k / fakt]);
 		
+		/* Pomičemo preostale brojeve */
 		for (j = k / fakt; j < n - 1; ++j)
 		{
 			br[j] = br[j + 1];
 		}
 		
+		/* Gledamo sljedeću manju faktorijelu */
 		k = k % fakt;
 		fakt = fakt / i;
 	}
 	
+	/* Zadnja znamenka je jednoznačno određena jer je samo jedna ostala u polju */
 	printf("%d\n", br[0]);
 	
 	return 0;
